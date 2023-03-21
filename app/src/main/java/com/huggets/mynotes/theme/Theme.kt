79250e930f6b93d46c9ba013c52a280b12504eff
Hugo Billerit <huggets@protonmail.com>
2023-03-21 22:26:59 +0100

    Optimize imports

--- app/src/main/java/com/huggets/mynotes/theme/Theme.kt
+++ app/src/main/java/com/huggets/mynotes/theme/Theme.kt
@@ -2,10 +2,9 @@ package com.huggets.mynotes.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
